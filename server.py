b7d7e7a67698c50182b6dd239bb34bcaf194ba69
Glenn Meyer <gmeyer1@ualberta.ca>
2015-03-01 18:47:04 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle
-# Glenn Meyer - gmeyer1
-# Zak Turchansky - zturchan
+# Copyright 2013 Abram Hindle, Glenn Meyer, Zak Turchansky
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
